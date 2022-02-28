d2a0b969d7cdb3ef25d6420dd55f814dc7091e63
happy2h <xienick520lsy@gmail.com>
2022-02-28 09:12:21 +0800

    add qq mail

--- mail.py
+++ mail.py
@@ -3,7 +3,7 @@ import yagmail
 
 def sendmail(data):
     try:
-        yag = yagmail.SMTP(user='xxx@qq.com', password='SMTP生成的临时密码', host='smtp.qq.com')
+        yag = yagmail.SMTP(user='3555232441@qq.com', password='jkhufenzzcccdcbd', host='smtp.qq.com')
         yag.send(to='你接收信息的邮箱', subject='健康打卡', contents=data)
         print('Email send success')
     except:
